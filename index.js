92d6cb5450b0d967ca5b6bac1d2c7470341b2c6a
deepaliphogat <112332304+deepaliphogat@users.noreply.github.com>
2022-12-07 23:14:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var typed = new Typed('#mytext',{
+    strings : ['JS Meals Website'],
+    typeSpeed  : 100,
+})
+AOS.init({
+    duration:1000,
+})
+
+
+$.ajax({
+    type : 'get' ,
+    url : 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian' ,
+
+    success : function(response)
+    {
+        for(var i=0 ; i<response.meals.length ; i++)
+        {
+
+            var newitem = `<div class='col-md-3 m-2 shadow-lg p-3 mb-5 bg-white rounded'>
+            <p>${response.meals[i].strMeal}<p/>
+            <img src=${response.meals[i].strMealThumb} class='img-fluid'/>
+            <p>${response.meals[i].idMeal}</p>
+            </div>`
+
+            $('#myitems').append(newitem)
+        }
+    },
+    
+    error : function(error)
+    {
+        console.log(error)
+    }
+})
